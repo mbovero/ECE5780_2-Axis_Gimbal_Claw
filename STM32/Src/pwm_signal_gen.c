06a30295718aff8eb919a3bb68ff2c66ec5ab2c7
BurkeDambly <burkeadambly@gmail.com>
2025-03-23 11:50:54 -0600

    red led on PWM with flux duty cycle

--- STM32/Src/pwm_signal_gen.c
+++ STM32/Src/pwm_signal_gen.c
@@ -6,27 +6,65 @@ int pwm_signal_gen(void) {
     
 HAL_Init(); // Reset of all peripherals, init the Flash and Systick
 
-SystemClock_Config(); //Configure the system clock
+//SystemClock_Config(); //Configure the system clock
 
-__HAL_RCC_GPIOC_CLK_ENABLE(); // Enable the GPIOC clock in the RCC
-
-
-// Configure PC7 as output
-GPIO_InitTypeDef GPIO_InitStruct; 
-GPIO_InitStruct.Pin = GPIO_PIN_7 | GPIO_PIN_6;
-GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-GPIO_InitStruct.Pull = GPIO_NOPULL;
-GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+__HAL_RCC_GPIOC_CLK_ENABLE(); 
+__HAL_RCC_TIM3_CLK_ENABLE();
 
 
 
-HAL_GPIO_WritePin(GPIOC, GPIO_PIN_6, GPIO_PIN_SET); // Start PC6 high
+GPIO_InitTypeDef GPIO_InitStruct_Red_LED; 
+GPIO_InitStruct_Red_LED.Pin = GPIO_PIN_6;
+GPIO_InitStruct_Red_LED.Mode = GPIO_MODE_AF_PP;
+GPIO_InitStruct_Red_LED.Pull = GPIO_NOPULL;
+GPIO_InitStruct_Red_LED.Speed = GPIO_SPEED_FREQ_LOW;
+GPIO_InitStruct_Red_LED.Alternate = GPIO_AF0_TIM3;
+HAL_GPIO_Init(GPIOC, &GPIO_InitStruct_Red_LED);
+
+GPIO_InitTypeDef GPIO_InitStruct_Blue_LED; 
+GPIO_InitStruct_Blue_LED.Pin = GPIO_PIN_7;
+GPIO_InitStruct_Blue_LED.Mode = GPIO_MODE_OUTPUT_PP;
+GPIO_InitStruct_Blue_LED.Pull = GPIO_NOPULL;
+GPIO_InitStruct_Blue_LED.Speed = GPIO_SPEED_FREQ_LOW;
+HAL_GPIO_Init(GPIOC, &GPIO_InitStruct_Blue_LED);
 
 
+
+TIM3->PSC = 0x3E7;      // Prescaler: 8 MHz → 8 kHz
+TIM3->ARR = 0xA;        // Auto-reload: 8 kHz → 800 Hz
+
+// Clear bits to configure Channel 1 (PC6) as output
+TIM3->CCMR1 &= ~(0x3);            // Clear bits 1 and 0
+TIM3->CCMR1 |= (0x7 << 4);        // PWM Mode 2 for Channel 1 (bits 6–4)
+
+// Enable output on Channel 1
+TIM3->CCER |= 1;                  // Enable channel 1 output
+
+// Set duty cycle for Channel 1 (PC6)
+TIM3->CCR1 = 0x9; // or 0x9 depending on which block runs
+
+TIM3->CR1 |= 1; // Enable TIM3 counter (starts PWM)
+
+int down = 1;
 while (1) {
     HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_7);
-    HAL_Delay(500);
+    HAL_Delay(100);
+
+    if(down){
+        TIM3->CCR1 -= 1;
     } 
+    else{
+        TIM3->CCR1 += 1;
+    }
+
+    if(TIM3->CCR1 == 1){
+        down = 0;
+    }
+
+    if(TIM3->CCR1 == 9){
+        down = 1;
+    }
+
 }
 
+}
