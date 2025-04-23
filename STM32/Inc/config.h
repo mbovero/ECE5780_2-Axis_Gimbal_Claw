347bf136bcca79c0b99bb011532bb2a40988006b
mbovero <milesbovero@icloud.com>
2025-04-23 13:44:17 -0600

    Removed unused constants

--- STM32/Inc/config.h
+++ STM32/Inc/config.h
@@ -10,13 +10,7 @@
 #define ROLL_PWM_ARR    49
 #define YAW_PWM_ARR     99
 
-#define MAX_PSC     300  // Minimum prescalar (slowest speed)
-#define MIN_PSC     20    // Maximum prescalar (fastest speed)
-#define MAX_SPEED   1000  // Maximum speed input value
-
 #define REVERSE_ROLL_DIR    0
 #define REVERSE_YAW_DIR     1
-
-//TODO settings to reverse motor directions
 
 #endif // CONFIG_H
